c6a06ef9901fcdc47af78b1b22ddc54d8d507975
Bilal Basharat <bilalbisharat@gmail.com>
2023-11-23 00:43:35 +0500

    update navbar

--- quiz_hub/lib/models/NavBar.dart
+++ quiz_hub/lib/models/NavBar.dart
@@ -1,77 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_hub/localStorage/local_storage.dart';
 import 'package:quiz_hub/models/constants.dart';
+import 'package:quiz_hub/models/loginResponse.dart';
 
-class NavBar extends StatelessWidget {
+class NavBar extends StatefulWidget {
    NavBar({super.key});
 
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
   Constants constants = Constants();
 
+  localStorage localUser = localStorage();
   @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        // Remove padding
-        padding: EdgeInsets.zero,
-        children: [
-          const UserAccountsDrawerHeader(
-            accountName: Text('User Name'),
-            accountEmail: Text('example@gmail.com'),
-            // currentAccountPicture: CircleAvatar(
-            //   child: ClipOval(
-            //     child: Image.network(
-            //       'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
-            //       fit: BoxFit.cover,
-            //       width: 90,
-            //       height: 90,
-            //     ),
-            //   ),
-            // ),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: NetworkImage(
-                      'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg')),
-            ),
-          ),
-          ListTile(
-            leading: Image.asset("assets/icons/take_exam.png", color: const Color(0xff8523D9),),
-            title: const Text('Access Exam', style: SideBarTextStyle,),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Image.asset("assets/icons/exam-results.png", height: 30, color: const Color(0xff8523D9),),
-            title: const Text('Get Result', style: SideBarTextStyle,),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Image.asset("assets/icons/feedback.png", color: const Color(0xff8523D9),),
-            title: const Text('Share Feedback', style: SideBarTextStyle),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Image.asset("assets/icons/logout.png", color: const Color(0xff8523D9),),
-            title: const Text('Logout', style: SideBarTextStyle,),
-          ),
-          // Divider(),
-          // ListTile(
-          //   leading: Icon(Icons.settings),
-          //   title: Text('Settings'),
-          //   onTap: () => null,
-          // ),
-          // ListTile(
-          //   leading: Icon(Icons.description),
-          //   title: Text('Policies'),
-          //   onTap: () => null,
-          // ),
-          // Divider(),
-          // ListTile(
-          //   title: Text('Exit'),
-          //   leading: Icon(Icons.exit_to_app),
-          //   onTap: () => null,
-          // ),
-        ],
-      ),
+  Widget build(BuildContext context){
+      return Drawer(
+        child: FutureBuilder(
+          future:localStorage().getUser(),
+          builder: (context, snap){
+          if(snap.connectionState == ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(
+              ),
+            );
+          }
+          else {
+            return ListView(
+              // Remove padding
+              padding: EdgeInsets.zero,
+              children: [
+                UserAccountsDrawerHeader(
+                  accountName: Text(snap.data!.name.toString()),
+                  accountEmail: Text(snap.data!.email.toString()),
+
+                  decoration:const BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(
+                            'https://oflutter.com/wp-content/uploads/2021/02/profile-bg3.jpg')),
+                  ),
+                ),
+                ListTile(
+                  leading: Image.asset("assets/icons/take_exam.png",
+                    color: const Color(0xff8523D9),),
+                  title: const Text('Access Exam', style: SideBarTextStyle,),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    "assets/icons/exam-results.png", height: 30,
+                    color: const Color(0xff8523D9),),
+                  title: const Text('Get Result', style: SideBarTextStyle,),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Image.asset("assets/icons/feedback.png",
+                    color: const Color(0xff8523D9),),
+                  title: const Text('Share Feedback', style: SideBarTextStyle),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    "assets/icons/logout.png", color: const Color(0xff8523D9),),
+                  title: const Text('Logout', style: SideBarTextStyle,),
+                ),
+              ],
+            );
+          }
+          })
     );
   }
 }
