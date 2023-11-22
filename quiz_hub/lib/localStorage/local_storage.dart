1c49e9c74ff4187cd15873ecc9cbc58a40ab4413
Bilal Basharat <bilalbisharat@gmail.com>
2023-11-23 00:39:23 +0500

    use shared_preference to store login data

---
+++ quiz_hub/lib/localStorage/local_storage.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:quiz_hub/models/loginResponse.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class localStorage{
+  Future<bool> setUser(loginResponse user) async {
+    print(user.email);
+   SharedPreferences sp = await SharedPreferences.getInstance();
+
+   bool userName = await sp.setString('user', json.encode(user.toJson()));
+   // print(userName);
+   return userName;
+  }
+
+  Future<loginResponse> getUser() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    String? userName = await sp.getString('user');
+    print(userName);
+    return loginResponse.fromJson(json.decode(userName!));
+  }
+
+}
