785abb1d2cfbe5f3690d9dff6fd810416015564e
Bilal Basharat <bilalbisharat@gmail.com>
2023-11-22 20:12:20 +0500

    create login role class

---
+++ quiz_hub/lib/models/loginRole.dart
@@ -0,0 +1,70 @@
+class roleLogin{
+  Data? data;
+
+  roleLogin({this.data});
+
+  roleLogin.fromJson(Map<String, dynamic> json) {
+    data = json['data'] != null ? new Data.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
+class Data {
+  String? sId;
+  String? firstName;
+  String? lastName;
+  String? email;
+  String? password;
+  String? role;
+  bool? isVerified;
+  String? createdAt;
+  String? updatedAt;
+  int? iV;
+
+  Data(
+      {this.sId,
+        this.firstName,
+        this.lastName,
+        this.email,
+        this.password,
+        this.role,
+        this.isVerified,
+        this.createdAt,
+        this.updatedAt,
+        this.iV});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    firstName = json['firstName'];
+    lastName = json['lastName'];
+    email = json['email'];
+    password = json['password'];
+    role = json['role'];
+    isVerified = json['isVerified'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    iV = json['__v'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['firstName'] = this.firstName;
+    data['lastName'] = this.lastName;
+    data['email'] = this.email;
+    data['password'] = this.password;
+    data['role'] = this.role;
+    data['isVerified'] = this.isVerified;
+    data['createdAt'] = this.createdAt;
+    data['updatedAt'] = this.updatedAt;
+    data['__v'] = this.iV;
+    return data;
+  }
+}
