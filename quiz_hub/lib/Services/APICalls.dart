78c134bc69adbfbcc0f4fa1a13fd179371ab1a23
Bilal Basharat <bilalbisharat@gmail.com>
2023-11-25 01:27:53 +0500

    integrate Node JS APIs

--- quiz_hub/lib/Services/APICalls.dart
+++ quiz_hub/lib/Services/APICalls.dart
@@ -3,27 +3,48 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:quiz_hub/localStorage/local_storage.dart';
 import 'package:quiz_hub/models/loginResponse.dart';
+import 'package:quiz_hub/models/loginRole.dart';
 
 class APICalls {
   final localStorage _storage = localStorage();
-  Future<loginResponse> loginAPI(String email, String password) async{
-try{
+  Future<loginResponse> loginAPI(String email, String password) async {
+    try {
+      http.Response login =
+          await http.post(Uri.parse('http://localhost:3000/api/user/jwt'),
+              headers: {'Content-Type': 'application/json'},
+              body: json.encode({
+                'email': email,
+                'password': password,
+              }));
+      final data = json.decode(login.body.toString());
+      if (login.statusCode == 200) {
+        loginResponse loginUser = loginResponse.fromJson(data);
+        _storage.setUser(loginUser);
+        return loginUser;
+      } else {
+        throw data['error'];
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
 
-  http.Response login = await http.post(Uri.parse('http://localhost:3000/api/user/jwt'),
-      headers: {'Content-Type':'application/json'}, body: json.encode({
-    'email': email,
-    'password': password,
-  }));
-  final data = json.decode(login.body.toString());
-  if(login.statusCode == 200){
-    loginResponse loginUser = loginResponse.fromJson(data);
-    _storage.setUser(loginUser);
-  return loginUser;
-  }else{
-    throw data['error'];
+  Future<roleLogin> getUserRole(String token) async {
+    try {
+      http.Response newRole = await http.post(
+          Uri.parse('http://localhost:3000/api/user/role'),
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': 'Bearer $token'
+          },
+          body: json.encode({}));
+      final data = json.decode(newRole.body.toString());
+      // print(data);
+      // return role;
+      roleLogin userRole = roleLogin.fromJson(data);
+      return userRole;
+    } catch (e) {
+      rethrow;
+    }
   }
-  }catch(e){
-  rethrow;
-  }
-}
 }
