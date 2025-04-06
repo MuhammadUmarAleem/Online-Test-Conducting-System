5a11ef111580b6f3f4b24561fb6c2902fc762f70
Bilal Basharat <99986862+Bilal-Basharat@users.noreply.github.com>
2025-04-06 15:51:42 +0500

    Create setup_quiz.sh

---
+++ setup_quiz.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Initialize a quiz directory
+mkdir quiz-data
+touch quiz-data/questions.txt quiz-data/answers.txt
+echo "Quiz directory created. Add questions to quiz-data/questions.txt."
